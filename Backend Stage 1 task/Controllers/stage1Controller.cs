8743db2f00662d5251b3e248834c2b8340a2b6ad
DevOyinda <kudiratoyindamolad@gmail.com>
2022-10-28 11:49:02 +0100

    Add project files.

---
+++ Backend Stage 1 task/Controllers/stage1Controller.cs
@@ -0,0 +1,26 @@
+﻿using Backend_Stage_1_task.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Stage_1_task.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class stage1Controller : Controller
+    {
+        [HttpGet]
+        public IActionResult GetUserDetails()
+        {
+            var res = new Response() { 
+            slackUsername = "DevOyinda",
+            backend = true,
+            age = 25,
+            bio = "Woman in Tech"};
+
+            return Ok(res);
+        }
+    }
+}
---
+++ Backend Stage 1 task/Model/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Stage_1_task.Model
+{
+    public class Response
+    {
+        public string slackUsername { get; set; }
+
+        public bool backend { get; set; }
+
+        public int age { get; set; }
+
+        public string bio { get; set; }
+    }
+}
